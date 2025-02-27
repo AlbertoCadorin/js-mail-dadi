185abcb63de66fce040c9820f32943ea7ec075c6
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-02-27 16:32:46 +0100

    feat: created email list

--- js-mail/app.js
+++ js-mail/app.js
@@ -1,1 +1,12 @@
+// creare una lista
+const list = [
+    'taro@gmail.com',
+    'asia@gmail.com',
+    'ron@gmail.com',
+    'lillo@gmail.com',
+    'martino@gmail.com',
+    'gastone@gmail.com',
+    'lucy@gmail.com',
+    'jimmi@gmail.com'
+]
 
