1cd3114b0df57546a30a6ff4c6b80384edb551b7
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-02-27 17:25:37 +0100

    feat: comparison of numbers

--- js-dadi/app.js
+++ js-dadi/app.js
@@ -4,3 +4,14 @@ let computer = Math.floor(Math.random() * 6) + 1;
 // stampare i numeri generati
 console.log("player has obtained "+ player);
 console.log("computer has obtained " + computer);
+// confrontare i numeri 
+if (player > computer){
+    // se il numero del giocatore e maggiore, vince
+        console.log("Player wins"); // stampare il risultato
+    }else if (computer > player){
+    // altrimenti se il numero del computer e maggiore, vince
+        console.log("Computer wins"); // stampare il risultato
+    }else{
+    //  se i numeri sono uguali, pareggio
+        console.log("Draw"); // stampare il risultato
+    }
