3ed0ace1069d796bbed57b8f717930f30fcce58e
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-02-27 17:24:04 +0100

    feat: generate a number

---
+++ js-dadi/app.js
@@ -0,0 +1,6 @@
+// generare un numero da 1 a 6 per il giocatore e computer
+let player = Math.floor(Math.random() * 6) + 1;
+let computer = Math.floor(Math.random() * 6) + 1;
+// stampare i numeri generati
+console.log("player has obtained "+ player);
+console.log("computer has obtained " + computer);
