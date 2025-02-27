63f14613c1bf40db30867b2a49c01cbf36987098
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-02-27 16:36:05 +0100

    feat: created the cycle that checks all the email

--- js-mail/app.js
+++ js-mail/app.js
@@ -13,5 +13,19 @@ const list = [
 const emailUsers = prompt('Please enter your email');
 // variabile di controllo
 let isEmailPresent = false ;
+// creare un ciclo che controlli tutte le email della lista e le confronta con l'email che scrive lutente
+for (i = 0; i < list.length; i++){
+    const currentEmail = list[i];
 
+    if(currentEmail === emailUsers){
+        isEmailPresent = true ;
+        break; // interrompere il ciclo appena una email 
+    }
+}
+// stampa il risultato
+if (isEmailPresent){
+    console.log("the email ", emailUsers ," is present, you can enter");
+}else{
+    console.log("the email ", emailUsers," is not present, it cannot enter");
+}
 
